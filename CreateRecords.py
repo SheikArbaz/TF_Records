0b20443c70ba72fe025a1b476f5b26a8139a4515
arbazsheik <arbaz.sheik@jotter.ai>
2018-09-10 15:51:25 +0530

    commited the basic changes

---
+++ CreateRecords.py
@@ -0,0 +1,301 @@
+
+"""Converts data to TFRecord.
+"""
+
+from __future__ import absolute_import
+from __future__ import division
+from __future__ import print_function
+
+from collections import Counter
+from collections import namedtuple
+from datetime import datetime
+from scipy.misc import imresize
+
+import json
+import os.path
+import random
+import sys
+import threading
+
+import numpy as np
+import pickle as pkl
+import scipy.io as sio
+import tensorflow as tf
+
+tf.flags.DEFINE_string("out_dir", "output/",
+											 "Image directory.")
+tf.flags.DEFINE_string("src_dir", "source/",
+											 "Directory containing src images.")
+tf.flags.DEFINE_string("trg_dir", "target/",
+											 "Directory containing masks.")
+
+tf.flags.DEFINE_string("train_label_file", "datalist.txt",
+											 "Training text file.")
+tf.flags.DEFINE_string("tf_dir", "tfrecord", "Output data directory.")
+tf.flags.DEFINE_string("prefix", "", "Prefix of the tensorflow record files.")
+
+tf.flags.DEFINE_integer("train_shards", 8,
+												"Number of shards in training TFRecord files.")
+tf.flags.DEFINE_integer("num_threads", 8,
+												"Number of threads to preprocess the images.")
+
+FLAGS = tf.flags.FLAGS
+
+ImageMetadata = namedtuple("ImageMetadata",
+													 ["out_id", "src_id", "trg_id"])
+
+class ImageDecoder(object):
+	"""Helper class for decoding images in TensorFlow."""
+
+	def __init__(self):
+		# Create a single TensorFlow Session for all image decoding calls.
+		config = tf.ConfigProto()
+		config.gpu_options.visible_device_list= '0,1,2'
+		self._sess = tf.Session(config=config)
+
+		# TensorFlow ops for JPEG decoding.
+		self._encoded_jpeg = tf.placeholder(dtype=tf.string)
+		self._encoded_png = tf.placeholder(dtype=tf.string)
+		self._decode_jpeg = tf.image.decode_jpeg(self._encoded_jpeg, channels=3)
+		self._decode_png = tf.image.decode_png(self._encoded_png, channels=3)
+
+		self._decoded_jpeg = tf.placeholder(dtype=tf.uint8, shape=[None, None, None])
+		self._encode_jpeg = tf.image.encode_jpeg(self._decoded_jpeg)
+
+	def decode_jpeg(self, encoded_jpeg):
+		image = self._sess.run(self._decode_jpeg,
+													 feed_dict={self._encoded_jpeg: encoded_jpeg})
+		assert len(image.shape) == 3
+		assert image.shape[2] == 3
+		return image
+
+	def decode_png(self, encoded_png):
+		image = self._sess.run(self._decode_png,
+													 feed_dict={self._encoded_png: encoded_png})
+		assert len(image.shape) == 3
+		assert image.shape[2] == 3
+		return image
+
+	def encode_jpeg(self, decoded_image):
+		image = self._sess.run(self._encode_jpeg,
+													 feed_dict={self._decoded_jpeg: decoded_image})
+		return image
+
+def _int64_feature(value):
+	"""Wrapper for inserting an int64 Feature into a SequenceExample proto."""
+	return tf.train.Feature(int64_list=tf.train.Int64List(value=[value]))
+
+def _bytes_feature(value):
+	"""Wrapper for inserting a bytes Feature into a SequenceExample proto."""
+	return tf.train.Feature(bytes_list=tf.train.BytesList(value=[str(value)]))
+
+def _float_feature(values):
+	return tf.train.Feature(float_list=tf.train.FloatList(value=[a for a in values]))
+
+def _int64_feature_list(values):
+	"""Wrapper for inserting an int64 FeatureList into a SequenceExample proto."""
+	return tf.train.FeatureList(feature=[_int64_feature(v) for v in values])
+
+
+def _bytes_feature_list(values):
+	"""Wrapper for inserting a bytes FeatureList into a SequenceExample proto."""
+	return tf.train.FeatureList(feature=[_bytes_feature(v) for v in values])
+
+def _to_tf_example(image, decoder):
+	"""Builds a TF Example proto for an image triplet.
+
+	Args:
+		image: An ImageMetadata object.
+		decoder: An ImageDecoder object.
+
+	Returns:
+		A TF Example proto.
+	"""
+	with open(FLAGS.out_dir + image.out_id, "r") as f:
+		encoded_out = f.read()
+	with open(FLAGS.src_dir + image.src_id, "r") as f:
+		encoded_src = f.read()
+	with open(FLAGS.trg_dir + image.trg_id, "r") as f:
+		encoded_trg = f.read()
+	
+	try:
+		decoded_image = decoder.decode_jpeg(encoded_out)
+		decoder.decode_jpeg(encoded_src)
+		
+	# try:
+	# 	decoder.decode_png(encoded_human)
+	# except:
+	# 	print("Skipping file with invalid PNG Human data: %s" % image.image_id)
+	except (tf.errors.InvalidArgumentError, AssertionError):
+		print("Skipping file with invalid JPEG data: %s" % image.image_id)
+		print("Skipping file with invalid JPEG data: %s" % image.product_image_id)
+		return
+
+	height = decoded_image.shape[0]
+	width = decoded_image.shape[1]
+	
+	
+
+
+	tf_example = tf.train.Example(features=tf.train.Features(feature={
+				'out_id': _bytes_feature(image.out_id),
+				'src_id': _bytes_feature(image.src_id),
+				'trg_id': _bytes_feature(image.trg_id),
+				'out_image': _bytes_feature(encoded_out),
+				'src_image': _bytes_feature(encoded_src),
+				'trg_image': _bytes_feature(encoded_trg),
+				'height': _int64_feature(height),
+				'width': _int64_feature(width),
+				
+				}))
+
+	return tf_example
+
+
+def _process_image_files(thread_index, ranges, name, images, decoder,
+														num_shards):
+	"""Processes and saves a subset of images as TFRecord files in one thread.
+
+	Args:
+		thread_index: Integer thread identifier within [0, len(ranges)].
+		ranges: A list of pairs of integers specifying the ranges of the dataset to
+			process in parallel.
+		name: Unique identifier specifying the dataset.
+		images: List of ImageMetadata.
+		decoder: An ImageDecoder object.
+		num_shards: Integer number of shards for the output files.
+	"""
+	# Each thread produces N shards where N = num_shards / num_threads. For
+	# instance, if num_shards = 128, and num_threads = 2, then the first thread
+	# would produce shards [0, 64).
+	num_threads = len(ranges)
+	assert not num_shards % num_threads
+	num_shards_per_batch = int(num_shards / num_threads)
+
+	shard_ranges = np.linspace(ranges[thread_index][0], ranges[thread_index][1],
+														 num_shards_per_batch + 1).astype(int)
+	num_images_in_thread = ranges[thread_index][1] - ranges[thread_index][0]
+
+	counter = 0
+	for s in xrange(num_shards_per_batch):
+		# Generate a sharded version of the file name, e.g. 'train-00002-of-00010'
+		shard = thread_index * num_shards_per_batch + s
+		output_filename = "%s-%.5d-of-%.5d" % (name, shard, num_shards)
+		output_file = os.path.join(FLAGS.tf_dir, output_filename)
+		writer = tf.python_io.TFRecordWriter(output_file)
+
+		shard_counter = 0
+		images_in_shard = np.arange(shard_ranges[s], shard_ranges[s + 1], dtype=int)
+		for i in images_in_shard:
+			image = images[i]
+
+			tf_example = _to_tf_example(image, decoder)
+			if tf_example is not None:
+				writer.write(tf_example.SerializeToString())
+				shard_counter += 1
+				counter += 1
+
+			if not counter % 1000:
+				print("%s [thread %d]: Processed %d of %d items in thread batch." %
+							(datetime.now(), thread_index, counter, num_images_in_thread))
+				sys.stdout.flush()
+
+		writer.close()
+		print("%s [thread %d]: Wrote %d image pairs to %s" %
+					(datetime.now(), thread_index, shard_counter, output_file))
+		sys.stdout.flush()
+		shard_counter = 0
+	print("%s [thread %d]: Wrote %d image pairs to %d shards." %
+				(datetime.now(), thread_index, counter, num_shards_per_batch))
+	sys.stdout.flush()
+
+
+def _process_dataset(name, images, num_shards):
+	"""Processes a complete data set and saves it as a TFRecord.
+
+	Args:
+		name: Unique identifier specifying the dataset.
+		images: List of ImageMetadata.
+		num_shards: Integer number of shards for the output files.
+	"""
+	
+	# Shuffle the ordering of images. Make the randomization repeatable.
+	random.seed(12345)
+	random.shuffle(images)
+
+	# Break the images into num_threads batches. Batch i is defined as
+	# images[ranges[i][0]:ranges[i][1]].
+	num_threads = min(num_shards, FLAGS.num_threads)
+	spacing = np.linspace(0, len(images), num_threads + 1).astype(np.int)
+	ranges = []
+	threads = []
+	for i in xrange(len(spacing) - 1):
+		ranges.append([spacing[i], spacing[i + 1]])
+
+	# Create a mechanism for monitoring when all threads are finished.
+	coord = tf.train.Coordinator()
+
+	# Create a utility for decoding JPEG images to run sanity checks.
+	decoder = ImageDecoder()
+
+	# Launch a thread for each batch.
+	print("Launching %d threads for spacings: %s" % (num_threads, ranges))
+	for thread_index in xrange(len(ranges)):
+		args = (thread_index, ranges, name, images, decoder, num_shards)
+		t = threading.Thread(target=_process_image_files, args=args)
+		t.start()
+		threads.append(t)
+
+	# Wait for all the threads to terminate.
+	coord.join(threads)
+	print("%s: Finished processing all %d image pairs in data set '%s'." %
+				(datetime.now(), len(images), name))
+
+
+
+def _load_and_process_metadata(label_file):
+	"""Loads image metadata from a text file
+	Args:
+		label_file: txt file containing pair annotations.
+		
+	Returns:
+		A list of ImageMetadata.
+	"""
+	
+	image_pairs = open(label_file).read().splitlines()
+	image_metadata = []
+	
+	for item in image_pairs:
+		name1 = item.strip()
+		image_pair = [name1, name1[:-5]+'1.jpg_pad.jpg', name1[:-5]+'1.jpg_mask.jpg']
+		# print (image_pair)
+		image_metadata.append(ImageMetadata(image_pair[0], image_pair[1], image_pair[2]))
+		
+	print("Finished processing %d pairs for %d images in %s" %
+				(len(image_pairs), len(image_pairs), label_file))
+
+	return image_metadata
+
+
+def main(unused_argv):
+	def _is_valid_num_shards(num_shards):
+		"""Returns True if num_shards is compatible with FLAGS.num_threads."""
+		return num_shards < FLAGS.num_threads or not num_shards % FLAGS.num_threads
+
+	assert _is_valid_num_shards(FLAGS.train_shards), (
+			"Please make the FLAGS.num_threads commensurate with FLAGS.train_shards")
+
+	if not tf.gfile.IsDirectory(FLAGS.tf_dir):
+		tf.gfile.MakeDirs(FLAGS.tf_dir)
+
+	
+	# Load image metadata from label files.
+	train_dataset = _load_and_process_metadata(FLAGS.train_label_file)
+	
+	print(len(train_dataset))
+
+	_process_dataset(FLAGS.prefix + "train", train_dataset, FLAGS.train_shards)
+
+
+if __name__ == "__main__":
+	tf.app.run()
---
+++ ReadTFRecords.py
@@ -0,0 +1,238 @@
+from __future__ import absolute_import
+from __future__ import division
+from __future__ import print_function
+
+import collections
+import json
+import math
+import os
+import time
+import numpy as np
+import scipy.io as sio
+import tensorflow as tf
+from scipy.misc import imresize
+
+input_file_pattern = "./tfrecord/train-?????-of-00008"
+values_per_input_shard = 443
+num_preprocess_threads = 1
+batch_size = 1
+
+def process_image(encoded_out,
+									encoded_src,
+									encoded_trg,
+									is_training=True,
+									height=256,
+									width=192,
+									resize_height=256,
+									resize_width=192,
+									thread_id=0,
+									image_format="jpeg",
+									zero_one_mask=True,
+									different_image_size=False):
+	
+	
+	def image_summary(name, image):
+		return
+		
+	# Decode image into a float32 Tensor of shape [?, ?, 3] with values in [0, 1).
+	with tf.name_scope("decode", values=[encoded_out]):
+		if image_format == "jpeg":
+			image = tf.image.decode_jpeg(encoded_out, channels=3)
+			human = tf.image.decode_jpeg(encoded_src, channels=3)
+			prod_image = tf.image.decode_jpeg(encoded_trg, channels=3)
+		elif image_format == "png":
+			image = tf.image.decode_png(encoded_out, channels=3)
+			human = tf.image.decode_png(encoded_src, channels=3)
+			prod_image = tf.image.decode_png(encoded_trg, channels=3)
+		else:
+			raise ValueError("Invalid image format: %s" % image_format)
+
+	image = tf.image.convert_image_dtype(image, dtype=tf.float32)
+	human = tf.image.convert_image_dtype(human, dtype=tf.float32)
+	prod_image = tf.image.convert_image_dtype(prod_image, dtype=tf.float32)
+	image_summary("original_image", image)
+	image_summary("original_human", human)
+	image_summary("original_prod_image", prod_image)
+	
+
+	# Resize image.
+	assert (resize_height > 0) == (resize_width > 0)
+	if different_image_size:
+		image = tf.image.resize_images(image,
+																	 size=[height, width],
+																	 method=tf.image.ResizeMethod.BILINEAR)
+		human = tf.image.resize_images(human,
+																	 size=[height, width],
+																	 method=tf.image.ResizeMethod.BILINEAR)
+		prod_image = tf.image.resize_images(prod_image,
+																				size=[height, width],
+																				method=tf.image.ResizeMethod.BILINEAR)
+	else:
+		image = tf.image.resize_images(image,
+																	 size=[resize_height, resize_width],
+																	 method=tf.image.ResizeMethod.BILINEAR)
+		human = tf.image.resize_images(human,
+																	 size=[resize_height, resize_width],
+																	 method=tf.image.ResizeMethod.BILINEAR)
+		prod_image = tf.image.resize_images(prod_image,
+																				size=[resize_height, resize_width],
+																				method=tf.image.ResizeMethod.BILINEAR)
+
+
+	
+	image_summary("final_image", image)
+	image_summary("final_human", human)
+	image_summary("final_prod_image", prod_image)
+	
+	# Rescale to [-1,1] instead of [0, 1]
+	# image = (image - 0.5) * 2.0
+	# human = (human - 0.5) * 2.0
+	# prod_image = (prod_image - 0.5) * 2.0
+	
+		
+	
+	# human, green, blue= tf.split(3, 3, human)
+	return image, human, prod_image
+
+
+
+
+
+
+
+def parse_tf_example(serialized, stage=""):
+	features = tf.parse_single_example(
+			serialized,
+			features={
+					"out_id": tf.FixedLenFeature([], tf.string),
+					"src_id": tf.FixedLenFeature([], tf.string),
+					"trg_id": tf.FixedLenFeature([], tf.string),
+					"out_image": tf.FixedLenFeature([], tf.string),
+					"src_image": tf.FixedLenFeature([], tf.string),
+					"trg_image": tf.FixedLenFeature([], tf.string),
+					"height": tf.FixedLenFeature([], tf.int64),
+					"width": tf.FixedLenFeature([], tf.int64),
+			}
+	)
+	encoded_out = features["out_image"]
+	encoded_src = features["src_image"]
+	encoded_trg = features["trg_image"]
+
+	height = tf.cast(features["height"], tf.int32)
+	width = tf.cast(features["width"], tf.int32)
+	
+	return (encoded_out, encoded_src, encoded_trg, features["out_id"],features["src_id"],features["trg_id"])
+def prefetch_input_data(reader,
+												file_pattern,
+												is_training,
+												batch_size,
+												values_per_shard,
+												input_queue_capacity_factor=16,
+												num_reader_threads=1,
+												shard_queue_name="filename_queue",
+												value_queue_name="input_queue"):
+	"""Prefetches string values from disk into an input queue.
+
+	In training the capacity of the queue is important because a larger queue
+	means better mixing of training examples between shards. The minimum number of
+	values kept in the queue is values_per_shard * input_queue_capacity_factor,
+	where input_queue_memory factor should be chosen to trade-off better mixing
+	with memory usage.
+
+	Args:
+		reader: Instance of tf.ReaderBase.
+		file_pattern: Comma-separated list of file patterns (e.g.
+				/tmp/train_data-?????-of-00100).
+		is_training: Boolean; whether prefetching for training or eval.
+		batch_size: Model batch size used to determine queue capacity.
+		values_per_shard: Approximate number of values per shard.
+		input_queue_capacity_factor: Minimum number of values to keep in the queue
+			in multiples of values_per_shard. See comments above.
+		num_reader_threads: Number of reader threads to fill the queue.
+		shard_queue_name: Name for the shards filename queue.
+		value_queue_name: Name for the values input queue.
+
+	Returns:
+		A Queue containing prefetched string values.
+	"""
+	data_files = []
+	for pattern in file_pattern.split(","):
+		data_files.extend(tf.gfile.Glob(pattern))
+	if not data_files:
+		tf.logging.fatal("Found no input files matching %s", file_pattern)
+	else:
+		tf.logging.info("Prefetching values from %d files matching %s",
+										len(data_files), file_pattern)
+
+	if is_training:
+		filename_queue = tf.train.string_input_producer(
+				data_files, shuffle=True, capacity=16, name=shard_queue_name)
+		min_queue_examples = values_per_shard * input_queue_capacity_factor
+		capacity = min_queue_examples + 100 * batch_size
+		values_queue = tf.RandomShuffleQueue(
+				capacity=capacity,
+				min_after_dequeue=min_queue_examples,
+				dtypes=[tf.string],
+				name="random_" + value_queue_name)
+	else:
+		filename_queue = tf.train.string_input_producer(
+				data_files, shuffle=False, capacity=1, name=shard_queue_name)
+		capacity = values_per_shard + 3 * batch_size
+		values_queue = tf.FIFOQueue(
+				capacity=capacity, dtypes=[tf.string], name="fifo_" + value_queue_name)
+
+	enqueue_ops = []
+	for _ in range(num_reader_threads):
+		_, value = reader.read(filename_queue)
+		enqueue_ops.append(values_queue.enqueue([value]))
+	tf.train.queue_runner.add_queue_runner(tf.train.queue_runner.QueueRunner(
+			values_queue, enqueue_ops))
+	tf.summary.scalar(
+			"queue/%s/fraction_of_%d_full" % (values_queue.name, capacity),
+			tf.cast(values_queue.size(), tf.float32) * (1. / capacity))
+
+	return values_queue
+def build_input():
+	# Load input data
+	input_queue = prefetch_input_data(
+			tf.TFRecordReader(),
+			input_file_pattern,
+			is_training=True,
+			batch_size=batch_size,
+			values_per_shard=values_per_input_shard,
+			input_queue_capacity_factor=2,
+			num_reader_threads=num_preprocess_threads)
+
+	# Image processing and random distortion. Split across multiple threads
+	images_and_maps = []
+
+	for thread_id in range(num_preprocess_threads):
+		serialized_example = input_queue.dequeue()
+		(encoded_out, encoded_src, encoded_trg, out_id, src_id, trg_id) = parse_tf_example(serialized_example)
+
+		#Body Segment is Human now
+		(out_image, src_image, trg_image) = process_image(encoded_out, encoded_src, encoded_trg)
+
+		images_and_maps.append([out_image, src_image, trg_image, out_id, src_id, trg_id])
+
+	# Batch inputs.
+	queue_capacity = (7 * num_preprocess_threads *
+										batch_size)
+
+	return tf.train.batch_join(images_and_maps,
+														 batch_size=batch_size,
+														 capacity=queue_capacity,
+														 name="batch")
+
+def main():
+	(out_image, src_image, trg_image, out_id, src_id, trg_id)=build_input()
+	print(out_image.shape, src_image.shape, trg_image.shape)
+	coord = tf.train.Coordinator()
+	sess = tf.Session()
+	threads = tf.train.start_queue_runners(sess=sess, coord=coord)
+	m = sess.run(out_image)
+	print (np.unique(m))
+	
+if __name__ == "__main__":
+	main()
+	#tf.app.run()
